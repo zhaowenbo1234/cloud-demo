e40a99d205908a1da2659a222d87faf10d7b5da0
zhaowb <308539393@qq.com>
2018-07-27 23:27:24 +0800

    测试提交代码到码云

---
+++ src/main/java/com/example/clouddemo/CloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.clouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDemoApplication.class, args);
+    }
+}
