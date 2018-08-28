1b2856b25dd520260499c8cf265fd575bb4eca9a
zhaowenbo1234 <308539393@qq.com>
2018-08-28 21:14:28 +0800

    spring cloud hystrix 断路器

---
+++ cloud-hystrix/src/main/java/com/zhaowb/springcloud/cloudhystrix/controller/DeptController.java
@@ -0,0 +1,44 @@
+package com.zhaowb.springcloud.cloudhystrix.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.zhaowb.springcloud.cloudapi.entities.Dept;
+import com.zhaowb.springcloud.cloudhystrix.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DeptController {
+
+    @Autowired
+    private DeptService service;
+
+    @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+    public boolean add(@RequestBody Dept dept) {
+        return service.add(dept);
+    }
+
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "getDeptError")
+    public Dept get(@PathVariable("id") Long id) {
+        Dept dept = service.get(id);
+        if (dept == null){
+            throw new RuntimeException("dept is null id =" + id);
+        }
+        return service.get(id);
+    }
+
+    public Dept getDeptError(@PathVariable("id") Long id){
+        Dept dept = new Dept();
+        dept.setDeptno(id);
+        dept.setDname("该ID：" + id + "没有没有对应的信息,null--@HystrixCommand");
+        dept.setDb_source("no this database in MySQL");
+        return dept;
+    }
+
+    @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+    public List<Dept> list() {
+        return service.list();
+    }
+}
