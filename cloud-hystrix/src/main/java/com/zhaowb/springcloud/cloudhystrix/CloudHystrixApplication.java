b504af56bda52a48ec27b16687c35d0ee5c9a91f
zhaowenbo1234 <308539393@qq.com>
2018-08-29 11:20:03 +0800

    删除不必要的引用 Delete unnecessary references

--- cloud-hystrix/src/main/java/com/zhaowb/springcloud/cloudhystrix/CloudHystrixApplication.java
+++ cloud-hystrix/src/main/java/com/zhaowb/springcloud/cloudhystrix/CloudHystrixApplication.java
@@ -6,14 +6,12 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEurekaClient
 @EnableHystrix
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 public class CloudHystrixApplication {
 
     public static void main(String[] args) {
